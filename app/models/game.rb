4b71eb88dbbbce006ac361290504c0d6bf2924e0
Demid Kashirin <demid.kashirin@gmail.com>
2020-06-12 13:25:12 +0300

    Add creating a game functionality

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
-  has_many :players, through: :player_game
+  VALID_COUNTS = [3, 4].freeze
+
+  has_many :player_games, dependent: :destroy
+  has_many :players, through: :player_games
+  accepts_nested_attributes_for :player_games
+
+  validates :players_count, presence: true, inclusion: { in: VALID_COUNTS }
 end
